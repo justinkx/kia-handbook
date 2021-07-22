d7db73a773483010531447bfc2434c51e6a86a3e
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-07-22 11:53:42 +0530

    created project folder scaffold

---
+++ App/Screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React, { memo } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+
+export default memo(HomeScreen);
+
+const styles = StyleSheet.create({});
