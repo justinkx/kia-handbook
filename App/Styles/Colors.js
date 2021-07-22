46722f1dd561f0bc5d2ca87634c5639239d9810b
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-07-22 12:29:23 +0530

    added global colors

---
+++ App/Styles/Colors.js
@@ -0,0 +1,11 @@
+export default COLORS = {
+  white: "#fffff",
+  black: "#00000",
+  intense_red: "#b2021f",
+  glacier_white_pearl: "#f3f9fc",
+  steel_silver: "#aeadad",
+  gravity_gray: "#363738",
+  auroa_black_pearl: "#161719",
+  intelligency_blue: "#0b21b3",
+  punchy_orange: "#f05204",
+};
