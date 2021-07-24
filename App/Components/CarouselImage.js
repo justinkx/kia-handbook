b587d8379ec5ee979d41d18a94c7dbb1ceb3578c
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-07-24 17:25:15 +0530

    added xtranslate

--- App/Components/CarouselImage.js
+++ App/Components/CarouselImage.js
@@ -4,7 +4,7 @@ import { StyleSheet, Image, View, Animated, Dimensions } from "react-native";
 import GlobalStyles from "../Styles/GlobalStyle";
 
 const { width, height } = Dimensions.get("window");
-const IMAGE_VIEW_WIDTH = width - 30;
+const IMAGE_VIEW_WIDTH = width;
 
 const CarouselImage = ({ item, scrollX, index }) => {
   const inputRange = useMemo(
@@ -18,31 +18,33 @@ const CarouselImage = ({ item, scrollX, index }) => {
 
   const scale = scrollX.interpolate({
     inputRange,
-    outputRange: [0.9, 1.2, 0.9],
+    outputRange: [0.9, 1.3, 0.9],
   });
 
   return (
     <View style={[{ width: IMAGE_VIEW_WIDTH }]}>
-      <Animated.View
+      <View
         style={[
           GlobalStyles.center,
           {
             height: height / 3.5,
-
+          },
+        ]}
+      >
+        <Animated.Image
+          source={{ uri: item }}
+          resizeMode="contain"
+          style={{
+            width: IMAGE_VIEW_WIDTH - 20,
+            height: height / 3.5,
             transform: [
               {
                 scale,
               },
             ],
-          },
-        ]}
-      >
-        <Image
-          source={{ uri: item }}
-          resizeMode="contain"
-          style={{ width: IMAGE_VIEW_WIDTH, height: height / 3.5 }}
+          }}
         />
-      </Animated.View>
+      </View>
     </View>
   );
 };
