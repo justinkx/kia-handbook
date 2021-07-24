48041325acb4899e9daf82bd48068aec21feb1d5
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-07-24 23:54:32 +0530

    interpolate colours

--- App/Components/ModelsCard.js
+++ App/Components/ModelsCard.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Dimensions } from "react-native";
 import Animated, {
   useAnimatedStyle,
   interpolate,
+  interpolateColor,
 } from "react-native-reanimated";
 
 const ITEM_SPACING = 15;
@@ -36,39 +37,44 @@ const ModelsCard = ({ item, index, scrollX }) => {
             (index - 1) * ITEM_WIDTH,
             index * ITEM_WIDTH,
           ],
-          [1, 1.5, 1]
+          [1, 1.7, 1]
         ),
       },
-    ],
-  }));
-
-  const maskedScale = useAnimatedStyle(() => ({
-    transform: [
       {
-        scale: interpolate(
+        translateX: interpolate(
           scrollX.value,
           [
             (index - 2) * ITEM_WIDTH,
             (index - 1) * ITEM_WIDTH,
             index * ITEM_WIDTH,
           ],
-          [1, 1.15, 1]
+          [-20, -10, 0]
         ),
       },
     ],
   }));
 
+  const animatedTitleStyle = useAnimatedStyle(() => ({
+    color: interpolateColor(
+      scrollX.value,
+      [(index - 2) * ITEM_WIDTH, (index - 1) * ITEM_WIDTH, index * ITEM_WIDTH],
+      ["#6b0f1a", "#923cb5", "#861657"]
+    ),
+  }));
+
   return (
     <View style={styles.container}>
       <Animated.View style={[styles.animatedView, animatedViewStyle]}>
-        <Animated.View style={[styles.maskedView, maskedScale]} />
-
+        <View style={[styles.maskedLeftView]} />
+        <View style={[styles.maskedRightView]} />
         <Animated.Image
           source={{ uri: item.images[0] }}
           style={[styles.image, animatedScale]}
           resizeMode="contain"
         />
-        <Text style={styles.name}>{item.name}</Text>
+        <Animated.Text style={[styles.name, animatedTitleStyle]}>
+          {item.name}
+        </Animated.Text>
         <Animated.Text>{item.description}</Animated.Text>
       </Animated.View>
     </View>
@@ -95,24 +101,34 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.32,
     shadowRadius: 5.46,
     elevation: 9,
+    zIndex: 10,
   },
   name: {
     fontSize: 16,
     fontWeight: "700",
+    paddingBottom: 4,
   },
   image: {
     width: "100%",
     height: 175,
+    zIndex: 10,
   },
-  maskedView: {
+  maskedLeftView: {
     backgroundColor: "white",
-    width: ITEM_WIDTH + ITEM_SPACING,
-    height: 170,
+    width: ITEM_WIDTH / 2 + ITEM_SPACING,
+    height: 190,
     position: "absolute",
     top: -5,
     left: -ITEM_SPACING,
-    borderBottomLeftRadius: ITEM_WIDTH,
-    borderBottomRightRadius: ITEM_WIDTH / 2,
-    zIndex: -10,
+    zIndex: 1,
+  },
+  maskedRightView: {
+    backgroundColor: "white",
+    width: ITEM_WIDTH / 2 + ITEM_SPACING,
+    height: 160,
+    position: "absolute",
+    top: -5,
+    right: -ITEM_SPACING,
+    zIndex: 1,
   },
 });
