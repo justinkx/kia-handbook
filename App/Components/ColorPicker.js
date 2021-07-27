f0892aef6dc0228718abc4807d783a019aa40851
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-07-27 12:18:33 +0530

    added selected color checkmark

--- App/Components/ColorPicker.js
+++ App/Components/ColorPicker.js
@@ -7,6 +7,8 @@ import {
   Text,
 } from "react-native";
 import _size from "lodash/size";
+import { Entypo } from "@expo/vector-icons";
+
 import GlobalStyle from "../Styles/GlobalStyle";
 
 const { width } = Dimensions.get("window");
@@ -25,6 +27,7 @@ const ColorPicker = ({ colors, selectedColor, onSelectColor }) => {
             pallette={color}
             key={index}
             size={_size(colors)}
+            selectedColor={selectedColor}
           />
         ))}
       </View>
@@ -34,40 +37,47 @@ const ColorPicker = ({ colors, selectedColor, onSelectColor }) => {
 
 export default memo(ColorPicker);
 
-const ColorPellette = memo(({ pallette, onSelectColor, size }) => {
-  const PALLETTE_SIZE = useMemo(
-    () => Math.min((PICKER_SIZE - MARGIN_RIGHT * size) / size, 20),
-    [size]
-  );
-  return (
-    <TouchableOpacity onPress={() => onSelectColor(pallette)}>
-      <View
-        style={[
-          styles.pallette,
-          {
-            backgroundColor: pallette.dualTone
-              ? pallette.colors[0]
-              : pallette.color,
-            width: PALLETTE_SIZE,
-            height: PALLETTE_SIZE,
-            borderRadius: PALLETTE_SIZE / 2,
-          },
-        ]}
-      >
-        {pallette.dualTone && (
-          <View
-            style={{
-              backgroundColor: pallette.colors[1],
-              width: PALLETTE_SIZE / 2,
+const ColorPellette = memo(
+  ({ pallette, onSelectColor, size, selectedColor }) => {
+    const PALLETTE_SIZE = useMemo(
+      () => Math.min((PICKER_SIZE - MARGIN_RIGHT * size) / size, 20),
+      [size]
+    );
+    return (
+      <TouchableOpacity onPress={() => onSelectColor(pallette)}>
+        <View
+          style={[
+            styles.pallette,
+            {
+              backgroundColor: pallette.dualTone
+                ? pallette.colors[0]
+                : pallette.color,
+              width: PALLETTE_SIZE,
               height: PALLETTE_SIZE,
-              position: "absolute",
-            }}
-          />
-        )}
-      </View>
-    </TouchableOpacity>
-  );
-});
+              borderRadius: PALLETTE_SIZE / 2,
+            },
+          ]}
+        >
+          {pallette.dualTone && (
+            <View
+              style={{
+                backgroundColor: pallette.colors[1],
+                width: PALLETTE_SIZE / 2,
+                height: PALLETTE_SIZE,
+                position: "absolute",
+              }}
+            />
+          )}
+          {selectedColor.paint === pallette.paint && (
+            <View style={styles.check}>
+              <Entypo name="check" size={15} color="#80ff80" />
+            </View>
+          )}
+        </View>
+      </TouchableOpacity>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   paint: {
@@ -88,5 +98,14 @@ const styles = StyleSheet.create({
     borderColor: "#00000033",
     position: "relative",
     overflow: "hidden",
+  },
+  check: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
