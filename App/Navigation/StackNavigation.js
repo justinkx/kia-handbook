3972c75ce6675a2dc4eee482ebdbb3cff588a18e
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-08-01 13:21:40 +0530

    added features screen

--- App/Navigation/StackNavigation.js
+++ App/Navigation/StackNavigation.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import HomeScreen from "../Screens/HomeScreen";
 import NAV_CONSTANTS from "./NavigationConstants";
 import DetailsScreen from "../Screens/DetailsScreen";
+import FeatureScreen from "../Screens/FeatureScreen";
 
 const Stack = createStackNavigator();
 
 export default HomeStack = () => {
+  const titleFromParamsOptions = useCallback(
+    ({ route }) => ({
+      title: route?.params?.title || "Features",
+    }),
+    []
+  );
+
   return (
-    <Stack.Navigator headerMode="none">
-      <Stack.Screen name={NAV_CONSTANTS.HOME_SCREEN} component={HomeScreen} />
+    <Stack.Navigator>
       <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+        name={NAV_CONSTANTS.HOME_SCREEN}
+        component={HomeScreen}
+      />
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
         name={NAV_CONSTANTS.DETAILS_SCREEN}
         component={DetailsScreen}
+      />
+      <Stack.Screen
+        name={NAV_CONSTANTS.FEATURES_SCREEN}
+        component={FeatureScreen}
+        options={titleFromParamsOptions}
       />
     </Stack.Navigator>
   );
---
+++ App/Screens/FeatureScreen.js
@@ -0,0 +1,19 @@
+import React, { memo } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+
+import GlobalStyles from "../Styles/GlobalStyle";
+
+const FeatureScreen = () => {
+  return (
+    <View style={GlobalStyles.flex}>
+      <ScrollView
+        style={GlobalStyles.flex}
+        contentContainerStyle={GlobalStyles.pagePadding}
+      ></ScrollView>
+    </View>
+  );
+};
+
+export default memo(FeatureScreen);
+
+const styles = StyleSheet.create({});
